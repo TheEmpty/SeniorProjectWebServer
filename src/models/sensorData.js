ca863915f9a7dec0b52831df433d764ba5e4b4ba
Mohammad El-Abid <mohammad@el-abid.com>
2015-04-19 17:37:24 -0400

    Add indexies

--- src/models/sensorData.js
+++ src/models/sensorData.js
@@ -1,10 +1,10 @@
 module.exports = function(mongoose) {
   var schema = new mongoose.Schema({
     version: { type: Number, required: true, default: 1 },
-    userId:  { type: String, required: true },
+    userId:  { type: String, required: true, index: true },
     value:   { type: Number, required: true },
-    sensor:  { type: String, required: true },
-    time:    { type: Date,   required: true, default:  Date.now}
+    sensor:  { type: String, required: true, index: true },
+    time:    { type: Date,   required: true, default: Date.now, index: true}
   })
 
   mongoose.model('SensorData', schema);
