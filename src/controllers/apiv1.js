67db28246df88487d856ddc5316bfb812adb1baf
Mohammad El-Abid <mohammad@el-abid.com>
2015-04-22 23:07:58 -0400

    The charts are now based on real data, but time frame is static

--- src/controllers/apiv1.js
+++ src/controllers/apiv1.js
@@ -1,4 +1,5 @@
 var readParam = require('../middleware/readParams.js');
+var auth      = require('../middleware/authentication.js');
 
 module.exports = function(app) {
   var modelFromId = function(model, id) {
@@ -11,6 +12,32 @@ module.exports = function(app) {
 
     return prom;
   }
+
+  app.get('api_v1_get_sensor', '/api/v1/sensors/get', readParam, auth, function*(next) {
+    var sensor = this.params.sensor;
+
+    var start = new Date(0);
+    start.setUTCSeconds(this.params.start);
+
+    var end = new Date(0);
+    end.setUTCSeconds(this.params.end);
+
+    // get data
+    var _this = this;
+    var prom = new Promise(function(resolve, reject) {
+      _this.models.SensorData.
+      where('sensor').equals(sensor).
+      where('userId').equals(_this.session.user_id).
+      where('time').gte(start).lte(end).
+      exec(function(err, results) {
+        resolve(results);
+      });
+    });
+
+    var data = yield prom;
+    this.body = data
+    this.type = "application/json"
+  })
 
   app.put('api_v1_sensor', '/api/v1/sensor', readParam, function*(next) {
     var user = yield modelFromId(this.models.User, this.params.account)
