57f4c58075bfb5aeff39429fcab68bc19fff8e65
Mohammad El-Abid <mohammad@el-abid.com>
2015-04-29 08:14:51 -0400

    Don't require Mongoose entry for every env

--- src/middleware/mongoose.js
+++ src/middleware/mongoose.js
@@ -2,11 +2,14 @@ require('colors');
 
 module.exports = function(app, options) {
   var mongoose = require('mongoose');
-  var uri = options.urls[app.env];
+  var uri;
 
   if(process.env.MONGO_URI) {
     uri = process.env.MONGO_URI.replace(/"/g,'');
     console.info("Overriding Mongo URI in favor of MONGO_URI".red.bold)
+  } else {
+    // So they don't need to have an entry for env, if using env vars
+    uri = options.urls[app.env];
   }
 
   mongoose.connect(uri);
